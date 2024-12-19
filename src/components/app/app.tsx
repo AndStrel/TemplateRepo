15db4aa8c4e44bc2ea248b6b7711a609f5527843
Andrey Strelnikov <xoz9ih@outlook.com>
2024-12-19 12:16:10 +0300

    del in app

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,11 +1,9 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { HomePage, ProfilePage } from '../../pages';
-import { CountdownTimer } from '../сountdownTimer';
 
 export const App: React.FC = () => {
   return (
     <Router>
-      <CountdownTimer />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/profile" element={<ProfilePage />} />
